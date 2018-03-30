3652c3e849a4f6cc829afc36845eeb34a08043e0
themostfreeboy <361425474@qq.com>
2018-03-30 13:07:45 +0800

    添加代码

---
+++ MultiFormTest_1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiFormTest_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form2();
+            form2.ShowDialog();//模式对话框
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form form3 = new Form3();
+            form3.Show();//非模式对话框
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("对话框3(消息对话框)");//消息对话框
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MultiFormTest
+多对话框
